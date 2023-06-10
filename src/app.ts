0f4adbfa834547b1f40e010dcd0559bf5e090752
Alfredo <alfredobenitezjuanilla@gmail.com>
2023-06-10 18:01:44 +0200

    Add app ts

---
+++ src/app.ts
@@ -0,0 +1,24 @@
+import express from 'express';
+import morgan from 'morgan';
+import cors from 'cors';
+import createDebug from 'debug';
+import { sampleRouter } from './routers/sample.router';
+const debug = createDebug('W6:App');
+
+export const app = express();
+debug('Cargando la App Express :');
+
+const corsOptions = {
+  origin: '*',
+};
+app.use(morgan('dev'));
+app.use(cors(corsOptions));
+app.use(express.json());
+app.use((req, res, next) => {
+  next();
+});
+app.get('/', (req, res) => {
+  res.send('Bienvenido express');
+});
+
+app.use('/sample', sampleRouter);
